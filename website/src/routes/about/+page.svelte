46819f9daedc8d95c8088813d3dfa58af43b35f1
JoshQuaintance <hasianjoshua@gmail.com>
2023-12-14 20:30:08 +0000

    Added style to about page

--- website/src/routes/about/+page.svelte
+++ website/src/routes/about/+page.svelte
@@ -4,3 +4,16 @@
     <p>This website is made by Joshua Quaintance</p>
     <p>There will be more content here</p>
 </div>
+
+<style>
+	div {
+		@apply w-screen h-screen
+		@apply flex items-center justify-center
+	}
+
+	h1 {
+		@apply underline decoration-indig-500
+
+	}
+
+</style>
